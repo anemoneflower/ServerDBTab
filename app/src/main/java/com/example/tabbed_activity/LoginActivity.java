09c8c9dd497f9365b0489006498a38b87e279d4e
seoyun <sallylucky@naver.com>
2019-07-09 00:34:38 +0900

    LoginSucceed

--- app/src/main/java/com/example/tabbed_activity/LoginActivity.java
+++ app/src/main/java/com/example/tabbed_activity/LoginActivity.java
@@ -3,20 +3,26 @@ package com.example.tabbed_activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
     private Context mContext;
     private LoginButton btn_facebook_login;
-    private Button btn_custom_login;
+    //    private Button btn_custom_login;
     private LoginCallback mLoginCallback;
     private CallbackManager mCallbackManager;
 
@@ -31,20 +37,43 @@ public class LoginActivity extends AppCompatActivity {
 
         btn_facebook_login = (LoginButton) findViewById(R.id.btn_facebook_login);
         btn_facebook_login.setReadPermissions(Arrays.asList("public_profile", "email"));
-        btn_facebook_login.registerCallback(mCallbackManager, mLoginCallback);
 
-        btn_custom_login = (Button) findViewById(R.id.btn_custom_login);
-        btn_custom_login.setOnClickListener(new View.OnClickListener() {
+        btn_facebook_login.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View view) {
-                btn_facebook_login.performClick();
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.v("result", object.toString());
+
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        finish();
+                    }
+                });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                graphRequest.setParameters(parameters);
+                graphRequest.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e("LoginErr", error.toString());
             }
         });
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         mCallbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
     }
+
 }
--- app/src/main/java/com/example/tabbed_activity/TabFragment4.java
+++ app/src/main/java/com/example/tabbed_activity/TabFragment4.java
@@ -1,19 +1,13 @@
 package com.example.tabbed_activity;
 
 import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import java.security.MessageDigest;
 
 
 public class TabFragment4 extends Fragment {
@@ -22,7 +16,6 @@ public class TabFragment4 extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
 //
 //        mContext = getApplicationContext();
 //
